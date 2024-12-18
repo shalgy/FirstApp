d5dbef8f45cdd92bb5a392b0ebd76fabdb5d15bd
shalgy <lexashalgy1978@gmail.com>
2024-12-18 17:04:54 +0300

    Программа по теме Профессия C#-разработчик Язык C# Модуль 3. Основы C# и CLR. Основные понятия и типы данных

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,21 +7,14 @@ namespace FirstApp
         static void Main(string[] args)
         {
             const string MyName = "Alexey";
-
-            Console.WriteLine(MyName);
-
-            Console.WriteLine("\tПривет, мир");
-            Console.WriteLine("\tМне 46 лет");
-            Console.WriteLine("\tMy name is {0}", MyName);
-            Console.WriteLine("  {0}", MyName);
-            Console.WriteLine("\u0040");
-            Console.WriteLine("\u0023");
-            Console.WriteLine(true);
-            Console.WriteLine(false);
-            Console.WriteLine(5);
-            Console.WriteLine(0x0A);
-            Console.WriteLine(0b11);
-            Console.WriteLine(5.5);
+            const byte MyAge = 46;
+            const bool IsHavePet = true;
+            const double ShoeSize = 43.5;
+            Console.WriteLine("My name is {0}\n"+
+                              "My age is {1}\n"+
+                              "Do I have a pet? {2}\n" +
+                              "My shoe size is {3}",
+                              MyName, MyAge, IsHavePet, ShoeSize);
             Console.ReadKey();
         }
     }
