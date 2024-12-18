f4cdf4843a020371ae2d01f8bb184751dda10434
shalgy <lexashalgy1978@gmail.com>
2024-12-18 16:48:24 +0300

    Знакомимся с переменными

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Alexey";
+
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\tМне 46 лет");
+            Console.WriteLine("\tMy name is {0}", MyName);
+            Console.WriteLine("  {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine(5);
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
+            Console.ReadKey();
+        }
+    }
+}
