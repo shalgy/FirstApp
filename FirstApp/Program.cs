641e74bdaad3a76b0bece195f416ee58d6b9a163
shalgy <lexashalgy1978@gmail.com>
2024-12-18 17:43:04 +0300

    Немного откорректировал файл примера по изучению типов данных C#

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,6 +10,7 @@ namespace FirstApp
             const byte MyAge = 46;
             const bool IsHavePet = true;
             const double ShoeSize = 43.5;
+            //const float ShoeSize = 43.5f; или так чтобы программа ела чуть меньше памяти
             Console.WriteLine("My name is {0}\n"+
                               "My age is {1}\n"+
                               "Do I have a pet? {2}\n" +
