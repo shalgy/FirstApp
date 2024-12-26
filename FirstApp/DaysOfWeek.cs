26c0c52fd236dbb1591e936c2fe12854a8a3489b
shalgy <lexashalgy1978@gmail.com>
2024-12-26 15:39:13 +0300

    Добавление урока по перечеслениям

---
+++ FirstApp/DaysOfWeek.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+namespace FirstApp
+{
+    class MainClass
+    {
+        public object LovelyDayOfWeek(byte myDay)
+        {
+            //DaysOfWeek MyFavoriteDay;
+
+            //MyFavoriteDay = DaysOfWeek.Friday;
+            return (DaysOfWeek)myDay;
+            //Console.WriteLine(CurrentDay);
+        }
+    }
+
+    enum DaysOfWeek : byte
+    {
+        Monday=1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+}
--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,12 +10,15 @@ namespace FirstApp
             const byte MyAge = 46;
             const bool IsHavePet = true;
             const double ShoeSize = 43.5;
+            MainClass LovelyDay = new MainClass();
             //const float ShoeSize = 43.5f; или так чтобы программа ела чуть меньше памяти
             Console.WriteLine("My name is {0}\n"+
                               "My age is {1}\n"+
                               "Do I have a pet? {2}\n" +
-                              "My shoe size is {3}",
-                              MyName, MyAge, IsHavePet, ShoeSize);
+                              "My shoe size is {3}\n"+
+                              "My lovely day is {4}",
+                              MyName, MyAge, IsHavePet, ShoeSize, LovelyDay.LovelyDayOfWeek(5));
+            LovelyDay = null; 
             Console.ReadKey();
         }
     }
