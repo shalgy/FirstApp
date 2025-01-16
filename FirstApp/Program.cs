ae9ce97689fd7b948526a126b487c87a18179761
shalgy <lexashalgy1978@gmail.com>
2025-01-16 10:49:15 +0300

    Выполнение Задания 4.5.1

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Principal;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -38,16 +39,62 @@ namespace FirstApp
             //Console.ReadKey();
 
             //Задание 4.4.5 Кортежи
-            (string Name, string Type, double Age, int NameCount) Pet;
-            Console.Write("Введите имя Вашего питомца: ");
-            Pet.Name = Console.ReadLine();
-            Console.Write("Введите тип Вашего питомца (кошка, собака, хомяк и т.п.): ");
-            Pet.Type = Console.ReadLine();
-            Console.Write("Введите возраст с цифрами: ");
-            Pet.Age = Convert.ToDouble(Console.ReadLine());
-            Pet.NameCount = Pet.Name.Length;
-            Console.WriteLine("Кличка Вашего питомца: {0}\nТип: {1}\nВозраст: {2} года\nДлина имени: {3} символа", Pet.Name, Pet.Type, Pet.Age, Pet.NameCount);
+            //(string Name, string Type, double Age, int NameCount) Pet;
+            //Console.Write("Введите имя Вашего питомца: ");
+            //Pet.Name = Console.ReadLine();
+            //Console.Write("Введите тип Вашего питомца (кошка, собака, хомяк и т.п.): ");
+            //Pet.Type = Console.ReadLine();
+            //Console.Write("Введите возраст с цифрами: ");
+            //Pet.Age = Convert.ToDouble(Console.ReadLine());
+            //Pet.NameCount = Pet.Name.Length;
+            //Console.WriteLine("Кличка Вашего питомца: {0}\nТип: {1}\nВозраст: {2} года\nДлина имени: {3} символа", Pet.Name, Pet.Type, Pet.Age, Pet.NameCount);
 
+            //Задание 4.5.1
+        (string name, string lastname, string login, int loginlength, bool ispet, double age, string[] favcolors) user;
+            int usercount = 1;
+            do
+            {
+                Console.WriteLine("Ввод информации по пользователю: {0}", usercount);
+                Console.WriteLine("Введите имя");
+                user.name = Console.ReadLine();
+                Console.WriteLine("Введите фамилию");
+                user.lastname = Console.ReadLine();
+                Console.WriteLine("Введите логин");
+                user.login = Console.ReadLine();
+                user.loginlength = user.login.Length;
+
+                bool ispetset = false;
+                while (ispetset == false)
+                {
+                    Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                    string answear = Console.ReadLine();
+                    if (answear == "да" || answear == "Да")
+                    {
+                        user.ispet = true;
+                        ispetset = true;
+                    }
+                    else if (answear == "нет" || answear == "Нет")
+                    {
+                        user.ispet = false;
+                        ispetset = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ваш ответ не верный, пожалуйста отвечайте Да или Нет");
+                        continue;
+                    }
+                }
+                Console.WriteLine("Введите Ваш возраст");
+                user.age = Convert.ToDouble(Console.ReadLine());
+                user.favcolors = new string[3];
+                Console.WriteLine("Укажите три любимых цвета, пример: зеленый");
+                for (int numcol = 0; numcol < 3; numcol++)
+                {
+                    Console.WriteLine("Укажите {0} любимый цвет", numcol + 1);
+                    user.favcolors[numcol] = Console.ReadLine();
+                }
+                usercount++;
+            } while (usercount < 4);
         }
     }
 }
