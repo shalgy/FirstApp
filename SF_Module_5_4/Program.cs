878c14e5df30966d258745cc8599e965470b5a0f
shalgy <lexashalgy1978@gmail.com>
2025-01-18 02:41:51 +0300

    Начало модуля 5.5

---
+++ SF_Module_5_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Principal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SF_Module_5_4
+{
+    class Program
+    {
+        //static string strpoint = "..";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+            Echo(str, deep);
+            Console.ReadKey();
+        }
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+            Console.WriteLine("..." + modif);
+            //strpoint = strpoint + "..";
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+    }
+}
